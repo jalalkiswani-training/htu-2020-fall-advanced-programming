f8e4e5c91363f96454efccb1b9e4ed3c2a185178
Hana Rasheed <alrasheedhana@gmail.com>
2020-11-15 09:36:10 +0200

    ..

---
+++ class-examples/src/main/java/edu/htu/ap/week5/session1/interfaces/Test.java
@@ -0,0 +1,12 @@
+package edu.htu.ap.week5.session1.interfaces;
+
+public class Test {
+
+	public static void main(String[] args) {
+	B b= new B();
+	b.sayGoodBye();
+	b.sayHello();
+	b.sayHi();
+
+}
+}
