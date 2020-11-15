6900669fc3a4b493cacb0c002dc319e3ce569d77
Hana Rasheed <alrasheedhana@gmail.com>
2020-11-15 09:34:36 +0200

    ..

--- class-examples/src/main/java/edu/htu/ap/week5/session1/abstraction/Test.java
+++ class-examples/src/main/java/edu/htu/ap/week5/session1/abstraction/Test.java
@@ -1,6 +1,17 @@
 package edu.htu.ap.week5.session1.abstraction;
 
 public class Test {
+	public static void main(String[] args) {
+		// abstarct class cannot be instantiated using new 
+		//A a=new A();
+		
+		
+		B b=new B();
+		b.sayGoodBye();
+		b.sayHello();
+		
+		
+		
 
-
+}
 }
